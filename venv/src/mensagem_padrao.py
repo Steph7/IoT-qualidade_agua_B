06e1d86cab1a3e794a5b4c16416ff2b73351746a
Nuvem <Steph7@users.noreply.github.com>
2024-12-17 14:23:16 +0000

    atualizacao mensagem_padrao

--- venv/src/mensagem_padrao.py
+++ venv/src/mensagem_padrao.py
@@ -1,14 +1,14 @@
-sensores = {
-    'estacao_01': ['sensor_01', 'sensor_02','sensor_03', 'sensor_04', 'sensor_05'],
-    'estacao_02': ['sensor_06', 'sensor_07','sensor_08', 'sensor_09', 'sensor_10'],
-    'estacao_03': ['sensor_11', 'sensor_12','sensor_13', 'sensor_14', 'sensor_15'],
-    'estacao_04': ['sensor_16', 'sensor_17','sensor_18', 'sensor_19', 'sensor_20'],
-    'estacao_05': ['sensor_21', 'sensor_22','sensor_23', 'sensor_24', 'sensor_25'],
-    'estacao_06': ['sensor_26', 'sensor_27','sensor_28', 'sensor_29', 'sensor_30'],
-    'estacao_07': ['sensor_31', 'sensor_32','sensor_33', 'sensor_34', 'sensor_35'],
-    'estacao_08': ['sensor_36', 'sensor_37','sensor_38', 'sensor_39', 'sensor_40'],
-    'estacao_09': ['sensor_41', 'sensor_42','sensor_43', 'sensor_44', 'sensor_45'],
-}
+sensores = [
+    ('estacao_01', ['sensor_01', 'sensor_02','sensor_03', 'sensor_04', 'sensor_05', 'sensor_06']),
+    ('estacao_02', ['sensor_07','sensor_08', 'sensor_09', 'sensor_10', 'sensor_11', 'sensor_12']),
+    ('estacao_03', ['sensor_13','sensor_14', 'sensor_15', 'sensor_16', 'sensor_17', 'sensor_18']),
+    ('estacao_04', ['sensor_19','sensor_20', 'sensor_21', 'sensor_22', 'sensor_23', 'sensor_24']),
+    ('estacao_05', ['sensor_25','sensor_26', 'sensor_27', 'sensor_28', 'sensor_19', 'sensor_30']),
+    ('estacao_06', ['sensor_31','sensor_32', 'sensor_33', 'sensor_34', 'sensor_35', 'sensor_36']),
+    ('estacao_07', ['sensor_37','sensor_38', 'sensor_39', 'sensor_40', 'sensor_41', 'sensor_42']),
+    ('estacao_08', ['sensor_43','sensor_44', 'sensor_45', 'sensor_46', 'sensor_47', 'sensor_48']),
+    ('estacao_09', ['sensor_49','sensor_50', 'sensor_51', 'sensor_52', 'sensor_53', 'sensor_54'])
+]
 
 estacoes = [
     "BREPON",  #01
@@ -34,17 +34,24 @@ parametros_nome = [
 
 periodicidade = "15 minutos"
 
+mensagem_completa = []
 
-def criar_mensagem_incricao:
+def criar_mensagem_incricao():
     for i in range(len(estacoes)):
         for j in range(len(parametros_nome)):
             mensagem_padrao = {
-                {sensores[i]} : {estacoes[i]},
+                f"{sensores[i][0]}": f"{estacoes[i]}",
                 "sensors": [
                     {
-                        "sensor_id": {sensores[i][j]},
-                        "data_type": {parametro[i]},
+                        "sensor_id": f"{sensores[i][1][j]}",
+                        "data_type": f"{parametros_nome[j]}",
                         "data_interval": periodicidade
                     }
                 ]
             }
+            mensagem_completa.append(mensagem_padrao)
+
+# Exibindo as mensagens
+criar_mensagem_incricao()
+for msg in mensagem_completa:
+    print(msg)
